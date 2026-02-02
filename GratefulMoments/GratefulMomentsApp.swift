503e6948f9974cc4631443f6ef2ad7240dba45ec
Qayyax <qayyax@gmail.com>
2026-02-01 22:56:43 -0500

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-02-01.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
